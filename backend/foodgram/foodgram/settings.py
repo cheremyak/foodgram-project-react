e97c4bb674298517c9ceb6b8212e8eb8e21e8afc
leoneed <leoneed@mbp-leoneed.local>
2023-04-27 23:00:18 +0500

    server and frontend failed :( should i finish apis first and test em via postman?

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LOCAL_BUILD = False
+LOCAL_BUILD = True
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '*)l3kc2z$b(i%umtp7c2(83t+7ruq-rn=-@ap^hie3sg7*z4s9'
 
 DEBUG = True
 
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres',),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
