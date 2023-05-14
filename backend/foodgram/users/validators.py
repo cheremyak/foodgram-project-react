d9eed470d83b5fb2a89d7a8e525d113a3980347d
leoneed <leoneed@mbp-leoneed.local>
2023-05-14 09:14:18 +0500

    2nd review pep8 fix

--- backend/foodgram/users/validators.py
+++ backend/foodgram/users/validators.py
@@ -3,9 +3,9 @@ from django.core.validators import RegexValidator
 
 
 EN_RU_LETTERS_ONLY = RegexValidator(
-                '[A-zА-яЁё]',
-                'Допустимы только буквы кириллицы и латиницы'
-            )
+    '[A-zА-яЁё]',
+    'Допустимы только буквы кириллицы и латиницы'
+)
 
 
 def validate_username(data):
