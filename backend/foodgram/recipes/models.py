c138e692426eec8a4a8e0880b3cab6a4607b333e
leoneed <leoneed@mbp-leoneed.local>
2023-05-14 07:24:52 +0500

    cart

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -181,7 +181,6 @@ class Favorite(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='+',
         verbose_name='Пользователь'
     )
     recipe = models.ForeignKey(
@@ -195,6 +194,37 @@ class Favorite(models.Model):
         ordering = ('-id',)
         verbose_name = 'Избранный рецепт'
         verbose_name_plural = 'Избранные рецепты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_favorite'
+            )
+        ]
 
     def __str__(self):
         return 'Избранные рецепты'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    cart = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='carts',
+        verbose_name='Список покупок'
+    )
+
+    class Meta():
+        ordering = ('-id',)
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'cart'),
+                name='unique_shopping_cart'
+            )
+        ]
