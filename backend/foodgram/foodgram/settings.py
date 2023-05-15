4d2010f5f668f704138aa187164c377c8ed34de1
leoneed <leoneed@mbp-leoneed.local>
2023-05-15 22:12:19 +0500

    another attempt

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '*)l3kc2z$b(i%umtp7c2(83t+7ruq-rn=-@ap^hie3sg7*z4s9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
